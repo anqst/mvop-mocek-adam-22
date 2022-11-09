d9de7f52ab52cc24b6a670ff39421feaaa569bc1
anqst <mocek.ad.2020@skola.ssps.cz>
2022-11-09 18:21:12 +0100

    tailwind-design: fix headingStart and headingBig props on ItemBox

--- individual-works/tailwind-design/src/components/ItemBox.jsx
+++ individual-works/tailwind-design/src/components/ItemBox.jsx
@@ -3,12 +3,13 @@ import Item from "./Item";
 function ItemBox(props) {
 
     return (
-        <div className="flex flex-col gap-12 items-center">
-            <div className={`flex flex-col gap-2 items-center`}>
-                <h3 className="uppercase text-rose-600 font-bold">{props.preHeading}</h3>
-                <h2 className={`font-bold text-${props.headingBigger == true ? "5" : "3"}xl`}>{props.heading}</h2>
+        <div className={`flex flex-col gap-12 ${props.headingStart === true ? "items-start" : "items-center"}`}>
+            <div className={`flex flex-col gap-2 w-full  ${props.headingStart === true ? "items-start" : "items-center"}`}>
+                <h3 className="uppercase text-rose-600 font-bold max-w-[80%]">{props.preHeading}</h3>
+                <h2 className={`font-bold max-w-[80%] ${props.headingBigger == true ? "text-5xl" : "text-4xl"}`}>{props.heading}</h2>
             </div>
-            <div className={`grid grid-cols-${props.columns} gap-20`}>
+            {/* PROBLÉM: NEFUNGUJE NASTAVENÍ GRID-COLS- DYNAMICKY (VIZ DALŠÍ ŘÁDEK) */}
+            <div className={`grid grid-cols-${props.columns} gap-x-20 gap-y-10`}>
                 {
                     /* https://stackoverflow.com/questions/47616355/foreach-in-react-jsx-does-not-output-any-html */
 
