ff8f1841885c6ffee709a17e5ba483a2807f3a7d
Mocek Adam <mocek.ad.2020@skola.ssps.cz>
2022-11-28 15:14:13 +0100

    async-await-load: array.map()

--- individual-works/async-await-load/src/App.jsx
+++ individual-works/async-await-load/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 
 function App() {
   const [points, setPoints] = useState(0)
+  const [users, setUsers] = useState([])
   const [name, setName] = useState(null)
 
   async function delayPromise(duration) {
@@ -17,6 +18,19 @@ function App() {
 
     const data = await fetch('https://jsonplaceholder.typicode.com/users');
     const jsonData = await data.json();
+
+    const _users = [];
+
+    for (const record in jsonData) {
+      _users.push(jsonData[record].name);
+    }
+
+    console.log("_users: ", _users); // log all _users
+
+    setUsers(_users);
+
+    /* console.log("users: ", users); */ // won't work - must use useEffect to detect change of state variable
+
     const username = jsonData[Math.floor(Math.random() * jsonData.length)].name;
 
     // delay to see the loading prompt
@@ -29,6 +43,11 @@ function App() {
     getRandomUsername();
   }, [])
 
+  // useEffect that runs on every change of users array
+  useEffect(() => {
+    console.log("users: ", users);
+  }, [users])
+
   function handleOnClick(num) {
     setPoints((points) => points += num)
   }
@@ -40,6 +59,9 @@ function App() {
       <button type='button' className='bg-stone-500 py-2 px-4 text-gray-900' onClick={() => handleOnClick(-points)}>reset</button>
       <button type='button' className='bg-red-400 py-2 px-4 text-gray-900' onClick={() => handleOnClick(-1)}>remove point</button>
       <p>Random name: <span className='text-orange-600'>{name === null ? "loading..." : name}</span></p>
+      {users.map(function (user, i) {
+        return (<p>Name {i + 1} : <span className='text-blue-500'>{user}</span></p>)
+      })}
     </div>
   )
 }
