e72ab9f5b2dc7bee9674e7d6ce6295c716524607
Mocek Adam <mocek.ad.2020@skola.ssps.cz>
2022-12-05 14:14:12 +0100

    react-hooks-2: update to create items from count

--- individual-works/react-hooks-2/src/App.jsx
+++ individual-works/react-hooks-2/src/App.jsx
@@ -1,41 +1,38 @@
 import Item from "./components/Item"
-import { useState, useEffect } from "react"
+import { useState } from "react"
 
-function App() {
+function App2() {
 
-  const [items, setItems] = useState([]);
+    const [count, setCount] = useState(0);
 
-  function add() {
-    setItems((_items) => [..._items, _items.length + 1]);
-  }
-
-  function remove() {
-    if (items.length > 0) {
-      setItems((current) => current.filter((item) => item !== current.length));
+    function add() {
+        setCount(count + 1);
     }
-  }
 
-  useEffect(() => {
-    console.log("items have been changed to: ", items);
-  }, [items]);
+    function remove() {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    }
 
-
-  return (
-    <div className="w-[80%] flex flex-col items-center m-auto gap-12 py-10">
-      <h1 className="text-2xl">React - Dynamické komponenty</h1>
-      <div className="flex flex-row items-center gap-10">
-        <button type="button" className="btn btn-1" onClick={() => add()}>Přidat</button>
-        <button type="button" className="btn btn-2" onClick={() => remove()}>Odebrat</button>
-      </div>
-      <div className="grid grid-cols-4 gap-16">
-        {items.map(item => {
-          return (
-            <Item count={item} />
-          );
-        })}
-      </div>
-    </div>
-  )
+    return (
+        <div className="w-[80%] flex flex-col items-center m-auto gap-12 py-10">
+            <h1 className="text-2xl">React - Dynamické komponenty</h1>
+            <div className="flex flex-row items-center gap-10">
+                <button type="button" className="btn btn-1" onClick={() => add()}>Přidat</button>
+                <button type="button" className="btn btn-2" onClick={() => remove()}>Odebrat</button>
+            </div>
+            {count !== 0 ?
+                <div className="grid grid-cols-4 gap-16">
+                    {[...Array(count)].map((e, i) => {
+                        return <Item count={i + 1} />
+                    })}
+                </div>
+                :
+                <div className="mt-16"><p className="text-xl p-8 rounded-md bg-yellow-300">Vložte komponentu kliknutím na tlačítko "Přidat".</p></div>
+            }
+        </div>
+    )
 }
 
-export default App
+export default App2
