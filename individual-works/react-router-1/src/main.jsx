8df053791ebe92a29e74e84cc5d14b288cd0ea96
Mocek Adam <mocek.ad.2020@skola.ssps.cz>
2023-01-30 15:14:55 +0100

    react-router-1: literally a breaking change ok im going to sleep now a mimir

--- individual-works/react-router-1/src/main.jsx
+++ individual-works/react-router-1/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, Link, Outlet, RouterProvider } from "react-router-dom";
 import './index.css'
+import { createBrowserRouter, Link, Outlet, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error-page";
 import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
 import Contact from "./routes/contact";
